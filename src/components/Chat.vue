9e55ef1d1beb4601a10fef7b275f3528dcd22097
Liv Marx <oehrenmarks@gmail.com>
2019-05-09 13:46:25 -0400

    Set up Welcome component and add Chat component

---
+++ src/components/Chat.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="chat contaier">
+    <h2>
+      Chat
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="welcome container">
+   <h1 class="center-align">Liv Chat App</h1>
+   <div class="card">
+     <div class="card-content center-align">
+       <h2 class="teal-text">
+         Welcome
+       </h2>
+       <form @submit.prevent="enterChat">
+         <label for="name">Enter your name:</label>
+         <input type="text" name="name" v-model="name">
+         <button class="btn teal">Enter Chat</button>
+       </form>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data() {
+    return {
+      name: null,
+    };
+  },
+  methods: {
+    enterChat() {
+      return console.log(this.name);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.welcome {
+  max-width: 400px;
+}
+h2 {
+  font-size: 3em;
+}
+.welcome button {
+  margin: 30px;
+}
+</style>
